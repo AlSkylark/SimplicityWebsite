4bf3a3b4e747197541305db37078dc2f3541aa1b
Alex Castro <alxc119@gmail.com>
2021-07-03 16:51:03 +0100

    Another round of cleaning code and deleting unused bits I had whilst I was learning Angular.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminActivateGuard } from './adminactivate.guard';
-import { LoginComponent } from './login/login.component';
 import { MainComicComponent } from './main-comic/main-comic.component';
 import { LoadingComponent } from './loading/loading.component';
 import { LogsComponent } from './logs/logs.component';
 import { ArchiveComponent } from './archive/archive.component';
 
-export const routingComponents = [MainComicComponent, LoginComponent, LoadingComponent, LogsComponent, ArchiveComponent]
+export const routingComponents = [MainComicComponent, LoadingComponent, LogsComponent, ArchiveComponent]
 
 const routes: Routes = [
   {path:'', redirectTo:'1', pathMatch: 'full'},
-  {path:'admin', component:routingComponents[1]},
-  {path:'loading', component: routingComponents[2]},
-  {path:'logs', component: routingComponents[3]},
-  {path:'archive', component: routingComponents[4]},
+  {path:'loading', component: routingComponents[1]},
+  {path:'logs', component: routingComponents[2]},
+  {path:'archive', component: routingComponents[3]},
   {path:':pageNumber', component: routingComponents[0]}
 ];
 
